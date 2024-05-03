bc5d548fcc20a82d1b73050d9f00d07d922ae29b
João Medeiros <joaomedeiros@MacBook-Air-de-Thaisa.local>
2024-05-03 14:36:27 -0300

    Fixes the mapping for Id property

--- src/main/java/com/experimentation/fullspringproject/domain/PostDetails.java
+++ src/main/java/com/experimentation/fullspringproject/domain/PostDetails.java
@@ -3,13 +3,12 @@ package com.experimentation.fullspringproject.domain;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
